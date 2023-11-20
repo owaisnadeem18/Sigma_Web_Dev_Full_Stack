cb4bce944c438c0f15e77fcb88685a86542e61ad
owaisnadeem18 <108211200+owaisnadeem18@users.noreply.github.com>
2023-11-20 16:29:39 +0500

    Add files via upload

---
+++ Video - 54/script.js
@@ -0,0 +1,42 @@
+// var a = "owais";
+
+// var b = " nadeem";
+
+// console.log(a + b);
+
+// // JavaScript is a client side language used to add interactivity in our websites to perform different kind of works in our website . We can also make our website dynamic by using JavaScript
+
+// // Today It was a just formal introduction of JavaScript which was all about listening and focusing with keen interest and focus
+
+// // We will keep on discussing all these things on the regular basis
+
+// let x;
+
+// console.log(typeof x); // Interview Question: Any variable of let whose value is not declared is undefined by default
+
+// // if you write the same code , but with const in JavaScript is considered as (it wil throw an error)
+
+// b = 456;
+// c = "abcd";
+// d = b + c;
+// // when string got concatenated in our number . It becomes an overall number
+
+// console.log(typeof d);
+// console.log(typeof b);
+// console.log(typeof c);
+
+// Conditional If Else Statement:
+
+// Check either a number entered by a user is even or odd ?
+
+alert("Welcome to my website dear user");
+
+let a = prompt("Enter a number to ask whether it is even or odd : ");
+
+if (a % 2 == 0) {
+  document.write("The number entered by the user is " + a + "Is even Number");
+} else {
+  document.write("The number entered by the user is " + a + " Is Odd Number");
+}
+
+console.log();

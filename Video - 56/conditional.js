0fa159a9bb99ce9be696feeb2e517973936eaf14
owaisnadeem18 <108211200+owaisnadeem18@users.noreply.github.com>
2023-11-21 23:58:28 +0500

    Add files via upload

---
+++ Video - 56/conditional.js
@@ -0,0 +1,36 @@
+// Use of if-else to check the conditions in our programming languages
+
+// let age = prompt("Enter your age = ");
+
+// if (age >= 18) {
+//   console.log("You can drive");
+// } else {
+//   console.log("You can not drive");
+// }
+
+// Logical Operators are three
+
+// AND Operator => ALL TRUE then answer is TRUE
+// OR Operator => If only any one condition is "TRUE" then answer is "TRUE"
+// NOT Operator => If Condition is TRUE then answer is False & If Condition is FALSE then answer is TRUE
+
+// if (age < 0) {
+//   document.write("You are not even born");
+// } else if (age < 10) {
+//   document.write("You are too younger to drive");
+// } else if (age > 18) {
+//   document.write("You can drive");
+// } else {
+//   document.write("Please enter a valid age !!! ");
+// }
+
+// We have also discussed else and else-if ladder here in this video
+
+// Use of ternary operator here in this video:
+
+// "solve the same problem using ternary operator "
+// what is ternary operator ? If we want to write some lines of code in a same line without coding too much , then we can use our ternary operator in JS
+
+let age = prompt("Enter your age = ");
+
+age >= 18 ? console.log("You can drive") : console.log("you cannot drive");

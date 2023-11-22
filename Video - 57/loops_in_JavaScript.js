a378876d97217a38ea5b4c57de3f33f42cd066d4
owaisnadeem18 <108211200+owaisnadeem18@users.noreply.github.com>
2023-11-22 23:50:24 +0500

    Add files via upload

---
+++ Video - 57/loops_in_JavaScript.js
@@ -0,0 +1,41 @@
+// For Loop In JavaScript
+
+// for (let i = 0; i < 10; i++) {
+//   console.log(i + 1);
+// }
+
+// // For-In Loop
+
+// obj = {
+//   name: "Owais",
+//   Class: "3rd Year",
+//   Department: "BSCS",
+// };
+
+// for (let v in obj) {
+//   console.log(v);
+// }
+
+// // For-Of Loop
+
+// for (let c of "Husnain-Ali") {
+//   console.log(c);
+// }
+
+// While Loop:
+// We use this loop , when we need to check any of the Condition in our JavaScript looping
+
+// a = 1;
+
+// while (a <= 5) {
+//   console.log(a);
+//   a += 1;
+// }
+
+// "Do While Loop". It is a kind of loop which runs atleas tone time irrespective of either the condition is true or false
+
+a = 10;
+do {
+  console.log(a);
+  a++;
+} while (a <= 9);

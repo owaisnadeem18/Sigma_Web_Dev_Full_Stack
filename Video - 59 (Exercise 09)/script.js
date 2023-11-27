84d246d076ec874b63a919df4c5c9959718cdd09
owaisnadeem18 <108211200+owaisnadeem18@users.noreply.github.com>
2023-11-27 17:49:25 +0500

    Add files via upload

---
+++ Video - 59 (Exercise 09)/script.js
@@ -0,0 +1,3 @@
+// Exercise # 59 Task Explaination in our javaScript
+
+// We have to create a faulty calculator, which produces error on some inputs and do not work accordingly . It will give errors on 10% cases . Will further discuss the details at the time of completion of this exercise

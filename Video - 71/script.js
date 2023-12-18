38e13735b80e2cb2c2a0c061771fc9c89fce9d14
owaisnadeem18 <108211200+owaisnadeem18@users.noreply.github.com>
2023-12-18 23:59:18 +0500

    Add files via upload

---
+++ Video - 71/script.js
@@ -0,0 +1,14 @@
+//
+
+document.querySelector(".box").textContent;
+document.querySelector(".container").textContent;
+document.querySelector(".container").tagName;
+document.querySelector(".container").hasAttribute("style");
+document.querySelector(".container").removeAttribute("style");
+document.querySelector(".container").setAttribute("style", "display:inline");
+document.querySelector(".container").attributes;
+document.querySelector(".box").outerHTML;
+document.querySelector(".box").innerHTML;
+
+document.designMode = "on";
+document.querySelector(".box").dataset;

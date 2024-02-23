b2a4563e12e1bf78a26e3006fa0e62e2ad257381
owaisnadeem18 <108211200+owaisnadeem18@users.noreply.github.com>
2024-02-23 21:42:13 +0500

    Add files via upload

---
+++ Video - 121 ( Intro To Next JS )/first/app/about/page.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <h1 className="text-xl">I am About Page of Next JS</h1>
+      <ul>
+        <li>1st</li>
+        <li>2nd</li>
+        <li>3rd</li>
+        <li>4th</li>
+        <li>5th</li>
+      </ul>
+    </div>
+  );
+};
+
+export default page;
---
+++ Video - 121 ( Intro To Next JS )/first/app/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <>I am Owais Nadeem</>;
+};
+
+export default page;
